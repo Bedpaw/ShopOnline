e45fe0eaf66ac61564a271a23ed48f97d4aef931
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-07-16 13:38:09 +0200

    Add Product Controller with one GET method to get all products

---
+++ ShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Contracts;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopOnline.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public class ProductController : ControllerBase
+    {
+        /*private readonly IProductRepository _productRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        
+        public ProductController(IProductRepository productRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _productRepository= = ProductRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAuthors()
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                _logger.LogInfo($"{location}: Attempted Call");
+                var authors = await _productRepository.FindAll();
+                var response = _mapper.Map<IList<ProductDTO>>(authors);
+                _logger.LogInfo($"{location}: Successful");
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+            
+        }
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+
+            return $"{controller} - {action}";
+        }
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong. Please contact the Administrator");
+        }*/
+    }
+}
